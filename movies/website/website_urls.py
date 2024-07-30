6fd1043afc871b2ea6ecf50ca6756ee606026032
zippo <prochilo.salvatore@gmail.com>
2024-07-30 08:15:30 +0200

    Added User Profile page

--- movies/website/website_urls.py
+++ movies/website/website_urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .website_view import home, about, movie_detail_view
+from .website_view import home, about, movie_detail_view, user_detail_view
 
 urlpatterns = [
     path('', home, name='home'),
     path('about', about, name='about'),
     path('movie/<int:id>/', movie_detail_view, name='movie_detail'),
+    path('user/', user_detail_view, name='user_detail'),
 
 ]
--- movies/website/website_view.py
+++ movies/website/website_view.py
@@ -1,11 +1,11 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
-from movies.models import Movie
+from movies.models import Movie, UserRating
 
 
 def home(request):
-    movies_list = Movie.objects.all().order_by('title')
+    movies_list = Movie.objects.all().order_by('popularity')
 
     paginator = Paginator(movies_list, 10)
     page = request.GET.get('page', 1)
@@ -31,3 +31,12 @@ def movie_detail_view(request, id):
     movie = get_object_or_404(Movie, id=id)
 
     return render(request, 'movie_detail.html', {'movie': movie})
+
+
+def user_detail_view(request):
+    user = request.user
+    if not user.is_authenticated:
+        return redirect('login')
+    user_ratings = UserRating.objects.filter(user=request.user).select_related('movie').order_by('-rating')[:10]
+
+    return render(request, 'user_detail.html', {'user': user, 'user_ratings': user_ratings})
